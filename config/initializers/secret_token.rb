a6b586fb7f4845ee8eeba261bc8e6f5bbec9240f
Amrit Ayalur <a.ayalur@gmail.com>
2013-05-11 12:58:10 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mom::Application.config.secret_token = '5d47cad97a7183b9b8e0cae51c3d7bd416ae67f5f796525a70dda9b274b704fd12bff0117dc10faf20d8174b32987d9b8e4d218c25147a1a52299e64eb547f70'
