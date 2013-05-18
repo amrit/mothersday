10cda6864f5f461364f9ea126017f5afa3c8b17d
Amrit Ayalur <a.ayalur@gmail.com>
2013-05-18 14:56:59 -0500

    fix broken approve users flow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,8 +18,7 @@ class UsersController < ApplicationController
 
   def update 
     @users = User.where(:approved => false)
-    @users.update_all({:approved => params[:approved]}, {:id => params[:user_ids]})
-    @users.save
+    @users.update_all({:approved => true}, {:id => params[:user_ids]})
     flash[:notice] = "Approved users!"
     redirect_to admin_path
   end
